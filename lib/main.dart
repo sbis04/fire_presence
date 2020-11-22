5799fad14725bebfc6c31203a324e81efa28d4b4
Souvik Biswas <sbis1999@gmail.com>
2020-11-22 23:21:34 +0530

    Auto login from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
 import 'package:fire_presence/screens/login_page.dart';
+import 'package:fire_presence/screens/presence_page.dart';
+import 'package:fire_presence/utils/authentication.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    getUserInfo();
+  }
+
+  Future getUserInfo() async {
+    await getUser();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +33,8 @@ class MyApp extends StatelessWidget {
         fontFamily: 'GoogleSans',
       ),
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home:
+          (uid != null && authSignedIn != false) ? PresencePage(userName: userName) : LoginPage(),
     );
   }
 }
