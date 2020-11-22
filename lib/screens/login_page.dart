8cff16f08367cec477a377e656e5145243d74212
Souvik Biswas <sbis1999@gmail.com>
2020-11-22 10:43:37 +0530

    Add login page

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,3 +1,5 @@
+import 'package:fire_presence/res/custom_colors.dart';
+import 'package:fire_presence/screens/name_page.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,6 +10,81 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: CustomColors.firebaseNavy,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 20.0,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/firebase_logo.png',
+                      height: 180,
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Fire',
+                      style: TextStyle(
+                        color: CustomColors.firebaseYellow,
+                        fontSize: 40,
+                      ),
+                    ),
+                    Text(
+                      'Presence',
+                      style: TextStyle(
+                        color: CustomColors.firebaseOrange,
+                        fontSize: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Container(
+                  width: double.maxFinite,
+                  child: RaisedButton(
+                    color: CustomColors.firebaseOrange,
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => NamePage(),
+                        ),
+                      );
+                    },
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                      child: Text(
+                        'LOGIN',
+                        style: TextStyle(
+                          fontFamily: 'Raleway',
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.firebaseGrey,
+                          letterSpacing: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
