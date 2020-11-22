c979d78ab29e16efc3e140cdd18dd91321f33626
Souvik Biswas <sbis1999@gmail.com>
2020-11-22 22:06:09 +0530

    Add cloud function to update the presence

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const firestore = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +10,25 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.onUserStatusChange = functions.database
+  .ref("/{uid}/presence")
+  .onUpdate(async (change, context) => {
+    // Get the data written to Realtime Database
+    const isOnline = change.after.val();
+
+    // Then use other event data to create a reference to the
+    // corresponding Firestore document.
+    const userStatusFirestoreRef = firestore.doc(`users/${context.params.uid}`);
+
+    if (!isOnline) {
+      console.log("User went offline");
+      return userStatusFirestoreRef.update({
+        presence: false,
+        last_seen: Date.now(),
+      });
+    } else {
+      console.log("User is online");
+      return null;
+    }
+  });
