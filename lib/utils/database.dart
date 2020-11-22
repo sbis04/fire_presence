02ab0ec7441264761cdb1b3b4c4b5327acc93b75
Souvik Biswas <sbis1999@gmail.com>
2020-11-22 14:26:00 +0530

    Add database methods

---
+++ lib/utils/database.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fire_presence/model/user.dart';
+import 'package:fire_presence/utils/authentication.dart';
+import 'package:flutter/material.dart';
+
+class Database {
+  /// The main Firestore user collection
+  final CollectionReference userCollection = FirebaseFirestore.instance.collection('users');
+
+  storeUserData({@required String userName}) async {
+    DocumentReference documentReferencer = userCollection.doc(uid);
+
+    User user = User(
+      uid: uid,
+      name: userName,
+      presence: true,
+      lastSeenInEpoch: DateTime.now().millisecondsSinceEpoch,
+    );
+
+    var data = user.toJson();
+
+    await documentReferencer.set(data).whenComplete(() {
+      print("User data added");
+    }).catchError((e) => print(e));
+  }
+
+  retrieveUsers() async {}
+}
