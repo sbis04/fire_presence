7af263b703774598ecdadbe1002cec0a2f98ba3b
Souvik Biswas <sbis1999@gmail.com>
2020-11-22 14:25:22 +0530

    Saving data to firestore

--- lib/screens/name_page.dart
+++ lib/screens/name_page.dart
@@ -1,5 +1,7 @@
 import 'package:fire_presence/res/custom_colors.dart';
+import 'package:fire_presence/screens/presence_page.dart';
 import 'package:fire_presence/utils/authentication.dart';
+import 'package:fire_presence/utils/database.dart';
 import 'package:flutter/material.dart';
 
 class NamePage extends StatefulWidget {
@@ -8,9 +10,12 @@ class NamePage extends StatefulWidget {
 }
 
 class _NamePageState extends State<NamePage> {
+  Database database = Database();
+
   TextEditingController textController;
   FocusNode textFocusNode;
   bool _isEditing = false;
+  bool _isStoring = false;
 
   String _validateString(String value) {
     value = value.trim();
@@ -78,6 +83,7 @@ class _NamePageState extends State<NamePage> {
             ),
             child: Column(
               children: [
+                Row(),
                 Expanded(
                   child: Center(
                     child: Padding(
@@ -137,45 +143,68 @@ class _NamePageState extends State<NamePage> {
                     ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(left: 20.0, right: 20.0),
-                  child: Container(
-                    width: double.maxFinite,
-                    child: RaisedButton(
-                      color: CustomColors.firebaseOrange,
-                      onPressed: textController.text.isNotEmpty
-                          ? () {
-                              // Navigator.of(context).push(
-                              //   MaterialPageRoute(
-                              //     builder: (context) => ProfilePictureScreen(
-                              //       widget.phoneNumber,
-                              //       '${textControllerFirstName.text} ${textControllerLastName.text}',
-                              //     ),
-                              //   ),
-                              // );
-                            }
-                          : null,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
-                        child: Text(
-                          'CONTINUE',
-                          style: TextStyle(
-                            fontFamily: 'Raleway',
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: textController.text.isNotEmpty
-                                ? CustomColors.firebaseGrey
-                                : Colors.white24,
-                            letterSpacing: 2,
+                _isStoring
+                    ? Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: CircularProgressIndicator(
+                          valueColor: new AlwaysStoppedAnimation<Color>(
+                            CustomColors.firebaseOrange,
+                          ),
+                        ),
+                      )
+                    : Padding(
+                        padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                        child: Container(
+                          width: double.maxFinite,
+                          child: RaisedButton(
+                            color: CustomColors.firebaseOrange,
+                            onPressed: textController.text.isNotEmpty
+                                ? () async {
+                                    setState(() {
+                                      _isStoring = true;
+                                    });
+
+                                    await database
+                                        .storeUserData(userName: textController.text)
+                                        .whenComplete(
+                                          Navigator.of(context).pushReplacement(
+                                            MaterialPageRoute(
+                                              builder: (context) => PresencePage(
+                                                userName: textController.text,
+                                              ),
+                                            ),
+                                          ),
+                                        )
+                                        .catchError(
+                                          (e) => print('Error in storing data: $e'),
+                                        );
+
+                                    setState(() {
+                                      _isStoring = false;
+                                    });
+                                  }
+                                : null,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                              child: Text(
+                                'CONTINUE',
+                                style: TextStyle(
+                                  fontFamily: 'Raleway',
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: textController.text.isNotEmpty
+                                      ? CustomColors.firebaseGrey
+                                      : Colors.white24,
+                                  letterSpacing: 2,
+                                ),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                ),
               ],
             ),
           ),
